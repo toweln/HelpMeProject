52915a18e2e26d6ca86a32402eafa8787ed1f270
iamjaredlee <jared_lee@brown.edu>
2016-05-11 15:56:48 -0400

    Fixed chatsocket merge stuff

--- src/main/java/edu/brown/cs/HelpMe/chat/ChatWebSocketHandler.java
+++ src/main/java/edu/brown/cs/HelpMe/chat/ChatWebSocketHandler.java
@@ -1,7 +1,10 @@
 package edu.brown.cs.HelpMe.chat;
 
-import org.eclipse.jetty.websocket.api.*;
-import org.eclipse.jetty.websocket.api.annotations.*;
+import org.eclipse.jetty.websocket.api.Session;
+import org.eclipse.jetty.websocket.api.annotations.OnWebSocketClose;
+import org.eclipse.jetty.websocket.api.annotations.OnWebSocketConnect;
+import org.eclipse.jetty.websocket.api.annotations.OnWebSocketMessage;
+import org.eclipse.jetty.websocket.api.annotations.WebSocket;
 
 /**
  * Handles chat web socket
@@ -13,25 +16,19 @@ public class ChatWebSocketHandler {
 
 	/**
 	 * Message emitted when a user connects
-	 * 
+	 *
 	 * @param user user
 	 */
 	@OnWebSocketConnect
 	public void onConnect(Session user) throws Exception {
 		String username = "User" + Chat.nextUserNumber++;
 		Chat.userUsernameMap.put(user, username);
-<<<<<<< HEAD
-//		 Chat.broadcastMessage(sender = "Server",
-//		 msg = (username + " joined the chat"));
-=======
-		Chat.broadcastMessage(sender = "Server",
-				msg = (username + " joined the chat"));
->>>>>>> b44f0ee3de8de1dee0729a74d2401f8b40164966
+
 	}
 
 	/**
-	 * message emitted on close 
-	 * @param user user 
+	 * message emitted on close
+	 * @param user user
 	 * @param statusCode status code
 	 * @param reason reason
 	 */
@@ -39,13 +36,7 @@ public class ChatWebSocketHandler {
 	public void onClose(Session user, int statusCode, String reason) {
 		String username = Chat.userUsernameMap.get(user);
 		Chat.userUsernameMap.remove(user);
-<<<<<<< HEAD
-//		 Chat.broadcastMessage(sender = "Server",
-//		 msg = (username + " left the chat"));
-=======
-		Chat.broadcastMessage(sender = "Server",
-				msg = (username + " left the chat"));
->>>>>>> b44f0ee3de8de1dee0729a74d2401f8b40164966
+
 	}
 
 	/**
